fc18d3b1cc63573ba81da9ea06fab043cddc33b0
Feihong Hsu <feihong.hsu@gmail.com>
2021-08-07 21:51:22 -0500

    Replace Stream with Seq to read lines from file

--- file_quickstart.ml
+++ file_quickstart.ml
@@ -1,15 +1,16 @@
 let file = "cedict.txt"
 
-let line_stream_of_file file =
+let line_seq_of_file file =
   let in_channel = open_in file in
-  Stream.from (fun _ ->
+  Seq.unfold (fun () ->
     try
-      Some (input_line in_channel)
+      Some (input_line in_channel, ())
     with End_of_file ->
       close_in in_channel;
-      None)
+      None) ()
 
 let () =
-  line_stream_of_file file
-  |> Stream.iter (fun line ->
+  line_seq_of_file file
+  |> Seq.filter (fun line -> line.[0] <> '#')
+  |> Seq.iter (fun line ->
     print_endline line)
