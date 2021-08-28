602f21cf045ca48adb5bfe576fb6a150295a27fd
Feihong Hsu <feihong.hsu@gmail.com>
2021-08-28 16:29:50 -0500

    Report line and column for syntax errors

--- json_menhir/main.ml
+++ json_menhir/main.ml
@@ -1,6 +1,12 @@
 module Ast = Main__Ast
 
-let parse (s : string) : Ast.json =
+let get_position ({lex_curr_p = {pos_lnum; pos_cnum; pos_bol; _}; _} : Lexing.lexbuf) =
+  (pos_lnum, pos_cnum - pos_bol + 1)
+
+let parse (s : string) : (Ast.json, string) result =
   let lexbuf = Lexing.from_string s in
-  let ast = Parser.prog Lexer.read lexbuf in
-  ast
+  match Parser.prog Lexer.read lexbuf with
+  | exception Lexer.SyntaxError mesg ->
+    let line, col = get_position lexbuf in
+    Error (Printf.sprintf "Syntax error at %d, %d: %s" line col mesg)
+  | ast -> Ok ast
