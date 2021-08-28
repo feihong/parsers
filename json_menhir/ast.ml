50755f07799af8d16bc282ca3fc231c5ea2e3cc5
Feihong Hsu <feihong.hsu@gmail.com>
2021-08-28 14:29:46 -0500

    Add Ast.show function

--- json_menhir/ast.ml
+++ json_menhir/ast.ml
@@ -5,3 +5,19 @@ type json =
   | String of string
   | Array of json list
   | Object of (string * json) list
+
+let rec show json =
+  match json with
+  | Bool true -> "true"
+  | Bool false -> "false"
+  | Null -> "null"
+  | Number n -> string_of_float n
+  | String s -> Printf.sprintf "%S" s
+  | Array xs -> show_array xs
+  | Object xs -> show_object xs
+and show_array xs =
+  let body = xs |> List.map show |> String.concat ", " in
+  "[ " ^ body ^ " ]"
+and show_object xs =
+  let body = xs |> List.map (fun (k, v) -> "\"" ^ k ^ "\"" ^ ": " ^ show v) |> String.concat ", " in
+  "{ " ^ body ^ " }"
